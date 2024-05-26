c49e279cf3bdd76d7fa01956d0e0b71cb6e30a0f
Oliver Forral <intrepion@gmail.com>
2024-05-26 11:09:21 -0700

    Checking that navigation exists.

--- nextjs-dashboard/cypress/e2e/dashboard.cy.ts
+++ nextjs-dashboard/cypress/e2e/dashboard.cy.ts
@@ -1,5 +1,12 @@
 describe("dashboard spec", () => {
   it("passes", () => {
     cy.visit("/dashboard");
+    cy.get('[data-cy="main header"]').should("have.text", "Dashboard Page");
+    cy.get('[data-cy="side nav link invoices"]').click();
+    cy.get('[data-cy="main header"]').should("have.text", "Invoices Page");
+    cy.get('[data-cy="side nav link customers"]').click();
+    cy.get('[data-cy="main header"]').should("have.text", "Customers Page");
+    cy.get('[data-cy="side nav link home"]').click();
+    cy.get('[data-cy="main header"]').should("have.text", "Dashboard Page");
   });
 });
