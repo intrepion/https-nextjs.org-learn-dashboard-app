b9ab31683a2ea3d3b0bc49bc701c0ae1c72cd0d5
Oliver Forral <intrepion@gmail.com>
2024-05-26 11:09:49 -0700

    Creating the dashboard layout

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <p>Customers Page</p>;
+  return <p data-cy="main header">Customers Page</p>;
 }
--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -7,13 +7,24 @@ import {
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: "Home", href: "/dashboard", icon: HomeIcon },
   {
-    name: "Invoices",
+    dataCy: "side nav link home",
+    href: "/dashboard",
+    icon: HomeIcon,
+    name: "Home",
+  },
+  {
+    dataCy: "side nav link invoices",
     href: "/dashboard/invoices",
     icon: DocumentDuplicateIcon,
+    name: "Invoices",
   },
-  { name: "Customers", href: "/dashboard/customers", icon: UserGroupIcon },
+  {
+    dataCy: "side nav link customers",
+    href: "/dashboard/customers",
+    icon: UserGroupIcon,
+    name: "Customers",
+  },
 ];
 
 export default function NavLinks() {
@@ -23,9 +34,10 @@ export default function NavLinks() {
         const LinkIcon = link.icon;
         return (
           <a
-            key={link.name}
-            href={link.href}
             className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            data-cy={link.dataCy}
+            href={link.href}
+            key={link.name}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
