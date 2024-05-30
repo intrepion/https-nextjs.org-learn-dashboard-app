a1cf7f08b46d7ebc7f072729f8d4a0be93a94542
Oliver Forral <intrepion@gmail.com>
2024-05-30 05:29:41 -0700

    Checking for active link.

--- nextjs-dashboard/cypress/e2e/dashboard.cy.ts
+++ nextjs-dashboard/cypress/e2e/dashboard.cy.ts
@@ -2,11 +2,21 @@ describe("dashboard spec", () => {
   it("passes", () => {
     cy.visit("/dashboard");
     cy.get('[data-cy="main header"]').should("have.text", "Dashboard Page");
+    cy.get('[data-cy="side nav link home active"]').should("have.text", "Home");
     cy.get('[data-cy="side nav link invoices"]').click();
     cy.get('[data-cy="main header"]').should("have.text", "Invoices Page");
+    cy.get('[data-cy="side nav link invoices active"]').should(
+      "have.text",
+      "Invoices",
+    );
     cy.get('[data-cy="side nav link customers"]').click();
     cy.get('[data-cy="main header"]').should("have.text", "Customers Page");
+    cy.get('[data-cy="side nav link customers active"]').should(
+      "have.text",
+      "Customers",
+    );
     cy.get('[data-cy="side nav link home"]').click();
     cy.get('[data-cy="main header"]').should("have.text", "Dashboard Page");
+    cy.get('[data-cy="side nav link home active"]').should("have.text", "Home");
   });
 });
