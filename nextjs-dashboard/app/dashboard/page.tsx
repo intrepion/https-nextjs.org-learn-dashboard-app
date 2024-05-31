2b8ec3d13988789de4c6240a3a2c73dea82f4d06
Oliver Forral <intrepion@gmail.com>
2024-05-30 18:08:40 -0700

    Fetching data for <RevenueChart/>

--- nextjs-dashboard/app/dashboard/page.tsx
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -2,8 +2,11 @@ import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
+import { fetchRevenue } from "@/app/lib/data";
 
 export default async function Page() {
+  const revenue = await fetchRevenue();
+
   return (
     <main>
       <h1
@@ -29,7 +32,7 @@ export default async function Page() {
         className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8"
         data-cy="revenue chart and invoice list"
       >
-        {/* <RevenueChart revenue={revenue}  /> */}
+        <RevenueChart revenue={revenue} />
         {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
       </div>
     </main>
