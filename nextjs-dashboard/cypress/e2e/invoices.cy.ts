32202292df32e332f803e484f749e5e9966176be
Oliver Forral <intrepion@gmail.com>
2024-06-07 17:53:44 -0600

    Changed click() to trigger('click') in cypress tests.

--- nextjs-dashboard/cypress/e2e/invoices.cy.ts
+++ nextjs-dashboard/cypress/e2e/invoices.cy.ts
@@ -8,21 +8,23 @@ describe("invoices spec", () => {
     cy.get('[data-cy="row Emil Kowalski $542.46 pending"]');
     cy.get('[data-cy="textbox search"]').type("Evil");
     cy.get('[data-cy="row Evil Rabbit $6.66 pending"]');
-    cy.get('[data-cy="button create"]').click();
+    cy.get('[data-cy="button create"]').trigger("click");
     cy.get('[data-cy="breadcrumb Create Invoice"]');
     cy.get('[data-cy="select customer"]').select("Evil Rabbit");
     cy.get('[data-cy="textbox amount"]').type("420.69");
-    cy.get('[data-cy="radio status paid"]').click();
+    cy.get('[data-cy="radio status paid"]').trigger("click");
     cy.get('[data-cy="button create"]');
-    cy.get('[data-cy="button cancel"]').click();
+    cy.get('[data-cy="button cancel"]').trigger("click");
     cy.get('[data-cy="textbox search"]').type("Emil");
-    cy.get('[data-cy="button edit Emil Kowalski $542.46 pending"]').click();
+    cy.get('[data-cy="button edit Emil Kowalski $542.46 pending"]').trigger(
+      "click",
+    );
     cy.get('[data-cy="breadcrumb Edit Invoice"]');
     cy.get('[data-cy="select customer"]').select("Emil Kowalski");
     cy.get('[data-cy="textbox amount"]').type("960.24");
-    cy.get('[data-cy="radio status pending"]').click();
+    cy.get('[data-cy="radio status pending"]').trigger("click");
     cy.get('[data-cy="button edit"]');
-    cy.get('[data-cy="button cancel"]').click();
+    cy.get('[data-cy="button cancel"]').trigger("click");
     cy.get('[data-cy="textbox search"]').type("Emil");
     cy.get('[data-cy="button delete Emil Kowalski $542.46 pending"]');
   });
