d0bed8652d75aba0da1629618abb8fd8bfc32db3
Oliver Forral <intrepion@gmail.com>
2024-06-06 20:16:32 -0600

    Checking for CRUD actions.

--- nextjs-dashboard/cypress/e2e/invoices.cy.ts
+++ nextjs-dashboard/cypress/e2e/invoices.cy.ts
@@ -1,13 +1,29 @@
 describe("invoices spec", () => {
   it("passes", () => {
     cy.visit("/dashboard/invoices");
-    cy.get('[data-cy="invoice row a1baeb0c-8b4b-4b38-98eb-81e6fcf7c911"]');
-    cy.get('[data-cy="invoice row 0e437a90-e9cf-4d6b-b731-905c04aa674e"]');
-    cy.get('[data-cy="invoice row 244b1de1-6f9c-46e2-acc6-0a56f10fe5f4"]');
-    cy.get('[data-cy="invoice row df504635-e9ed-4363-a479-9e6a9a36c10b"]');
-    cy.get('[data-cy="invoice row dcbec06d-678c-4dae-bbd7-49c66d431954"]');
+    cy.get('[data-cy="row Delba de Oliveira $89.45 paid"]');
+    cy.get('[data-cy="row Steven Tey $448.00 paid"]');
+    cy.get('[data-cy="row Lee Robinson $5.00 paid"]');
+    cy.get('[data-cy="row Michael Novotny $345.77 pending"]');
+    cy.get('[data-cy="row Emil Kowalski $542.46 pending"]');
     cy.get('[data-cy="textbox search"]').type("Evil");
-    cy.get('[data-cy="invoice row dd1aed6f-374d-4318-88b9-277bd50f709e"]');
-    cy.get('[data-cy="button create invoice"]').click();
+    cy.get('[data-cy="row Evil Rabbit $6.66 pending"]');
+    cy.get('[data-cy="button create"]').click();
+    cy.get('[data-cy="breadcrumb Create Invoice"]');
+    cy.get('[data-cy="select customer"]').select("Evil Rabbit");
+    cy.get('[data-cy="textbox amount"]').type("420.69");
+    cy.get('[data-cy="radio status paid"]').click();
+    cy.get('[data-cy="button create"]');
+    cy.get('[data-cy="button cancel"]').click();
+    cy.get('[data-cy="textbox search"]').type("Emil");
+    cy.get('[data-cy="button edit Emil Kowalski $542.46 pending"]').click();
+    cy.get('[data-cy="breadcrumb Edit Invoice"]');
+    cy.get('[data-cy="select customer"]').select("Emil Kowalski");
+    cy.get('[data-cy="textbox amount"]').type("960.24");
+    cy.get('[data-cy="radio status pending"]').click();
+    cy.get('[data-cy="button edit"]');
+    cy.get('[data-cy="button cancel"]').click();
+    cy.get('[data-cy="textbox search"]').type("Emil");
+    cy.get('[data-cy="button delete Emil Kowalski $542.46 pending"]');
   });
 });
