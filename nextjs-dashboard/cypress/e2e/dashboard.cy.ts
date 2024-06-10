9fa925a405e0f949b9a798ebd8929fc0b2534883
Oliver Forral <intrepion@gmail.com>
2024-06-09 20:16:58 -0600

    Added longer delay.

--- nextjs-dashboard/cypress/e2e/dashboard.cy.ts
+++ nextjs-dashboard/cypress/e2e/dashboard.cy.ts
@@ -5,6 +5,7 @@ describe("dashboard spec", () => {
     cy.get('[data-cy="textbox email"]').type("user@nextmail.com");
     cy.get('[data-cy="textbox password"]').type("123456");
     cy.get('[data-cy="button log in"]').click({ force: true });
+    cy.wait(4000);
     cy.get('[data-cy="main header"]').should("have.text", "Dashboard");
     cy.get('[data-cy="side nav link home active"]').should("have.text", "Home");
     cy.get('[data-cy="side nav link invoices"]').click({ force: true });
@@ -30,6 +31,7 @@ describe("dashboard spec", () => {
     cy.get('[data-cy="textbox email"]').type("user@nextmail.com");
     cy.get('[data-cy="textbox password"]').type("123456");
     cy.get('[data-cy="button log in"]').click({ force: true });
+    cy.wait(4000);
     cy.get('[data-cy="invoice cards"]');
     cy.get('[data-cy="revenue chart and invoice list"]');
     cy.get('[data-cy="revenue chart"]');
