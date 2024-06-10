358c444c67d554074a7b7f9023dc6c8560dfea4c
Oliver Forral <intrepion@gmail.com>
2024-06-09 21:33:33 -0600

    Alphabetized config files.

--- nextjs-dashboard/cypress/e2e/invoices.cy.ts
+++ nextjs-dashboard/cypress/e2e/invoices.cy.ts
@@ -17,16 +17,23 @@ describe("invoices spec", () => {
     cy.get('[data-cy="textbox search"]').type("Evil");
     cy.get('[data-cy="row Evil Rabbit $6.66 pending"]');
     cy.get('[data-cy="button create"]').click({ force: true });
+    cy.wait(4000);
     cy.get('[data-cy="breadcrumb Create Invoice"]');
     cy.get('[data-cy="select customer"]').select("Evil Rabbit");
     cy.get('[data-cy="textbox amount"]').type("420.69");
     cy.get('[data-cy="radio status paid"]').click();
     cy.get('[data-cy="button create"]');
     cy.get('[data-cy="button cancel"]').click({ force: true });
+    cy.wait(4000);
     cy.get('[data-cy="textbox search"]').type("Emil");
-    cy.get('[data-cy="button edit Emil Kowalski $542.46 pending"]').click({
-      force: true,
-    });
+    cy.get('[data-cy="button edit Emil Kowalski $542.46 pending"]')
+      .invoke("attr", "href")
+      .then((href) => {
+        if (href !== undefined) {
+          cy.visit(href);
+        }
+      });
+    cy.wait(4000);
     cy.get('[data-cy="breadcrumb Edit Invoice"]');
     cy.get('[data-cy="select customer"]').select("Emil Kowalski");
     cy.get('[data-cy="textbox amount"]').type("960.24");
--- nextjs-dashboard/tailwind.config.ts
+++ nextjs-dashboard/tailwind.config.ts
@@ -6,17 +6,18 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  plugins: [require("@tailwindcss/forms")],
   theme: {
     extend: {
-      gridTemplateColumns: {
-        "13": "repeat(13, minmax(0, 1fr))",
-      },
       colors: {
         blue: {
           400: "#2589FE",
           500: "#0070F3",
           600: "#2F6FEB",
         },
+      },
+      gridTemplateColumns: {
+        "13": "repeat(13, minmax(0, 1fr))",
       },
     },
     keyframes: {
@@ -27,6 +28,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("@tailwindcss/forms")],
 };
+
 export default config;
