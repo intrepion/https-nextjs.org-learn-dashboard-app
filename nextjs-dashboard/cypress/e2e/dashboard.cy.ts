5b84dcd6ca2a3dace1b682d31cb2988f9d551167
Oliver Forral <intrepion@gmail.com>
2024-06-09 18:39:00 -0600

    Added force true to button clicks.

--- nextjs-dashboard/cypress/e2e/dashboard.cy.ts
+++ nextjs-dashboard/cypress/e2e/dashboard.cy.ts
@@ -4,24 +4,24 @@ describe("dashboard spec", () => {
     cy.get('[data-cy="logo"]');
     cy.get('[data-cy="textbox email"]').type("user@nextmail.com");
     cy.get('[data-cy="textbox password"]').type("123456");
-    cy.get('[data-cy="button log in"]').click();
+    cy.get('[data-cy="button log in"]').click({ force: true });
     cy.wait(1000);
     cy.visit("/dashboard");
     cy.get('[data-cy="main header"]').should("have.text", "Dashboard");
     cy.get('[data-cy="side nav link home active"]').should("have.text", "Home");
-    cy.get('[data-cy="side nav link invoices"]').click();
+    cy.get('[data-cy="side nav link invoices"]').click({ force: true });
     cy.get('[data-cy="main header"]').should("have.text", "Invoices");
     cy.get('[data-cy="side nav link invoices active"]').should(
       "have.text",
       "Invoices",
     );
-    cy.get('[data-cy="side nav link customers"]').click();
+    cy.get('[data-cy="side nav link customers"]').click({ force: true });
     cy.get('[data-cy="main header"]').should("have.text", "Customers Page");
     cy.get('[data-cy="side nav link customers active"]').should(
       "have.text",
       "Customers",
     );
-    cy.get('[data-cy="side nav link home"]').click();
+    cy.get('[data-cy="side nav link home"]').click({ force: true });
     cy.get('[data-cy="main header"]').should("have.text", "Dashboard");
     cy.get('[data-cy="side nav link home active"]').should("have.text", "Home");
   });
@@ -31,7 +31,7 @@ describe("dashboard spec", () => {
     cy.get('[data-cy="logo"]');
     cy.get('[data-cy="textbox email"]').type("user@nextmail.com");
     cy.get('[data-cy="textbox password"]').type("123456");
-    cy.get('[data-cy="button log in"]').click();
+    cy.get('[data-cy="button log in"]').click({ force: true });
     cy.wait(1000);
     cy.visit("/dashboard");
     cy.get('[data-cy="invoice cards"]');
--- nextjs-dashboard/cypress/e2e/login.cy.ts
+++ nextjs-dashboard/cypress/e2e/login.cy.ts
@@ -4,6 +4,6 @@ describe("home spec", () => {
     cy.get('[data-cy="logo"]');
     cy.get('[data-cy="textbox email"]').type("user@nextmail.com");
     cy.get('[data-cy="textbox password"]').type("123456");
-    cy.get('[data-cy="button log in"]').click();
+    cy.get('[data-cy="button log in"]').click({ force: true });
   });
 });
