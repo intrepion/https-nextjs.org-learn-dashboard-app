a89502a69370b8ec8ece39ac7b27d7dcb69044fa
Oliver Forral <intrepion@gmail.com>
2024-06-09 18:59:56 -0600

    Removed too many force clicks.

--- nextjs-dashboard/cypress/e2e/dashboard.cy.ts
+++ nextjs-dashboard/cypress/e2e/dashboard.cy.ts
@@ -9,19 +9,19 @@ describe("dashboard spec", () => {
     cy.visit("/dashboard");
     cy.get('[data-cy="main header"]').should("have.text", "Dashboard");
     cy.get('[data-cy="side nav link home active"]').should("have.text", "Home");
-    cy.get('[data-cy="side nav link invoices"]').click({ force: true });
+    cy.get('[data-cy="side nav link invoices"]').click();
     cy.get('[data-cy="main header"]').should("have.text", "Invoices");
     cy.get('[data-cy="side nav link invoices active"]').should(
       "have.text",
       "Invoices",
     );
-    cy.get('[data-cy="side nav link customers"]').click({ force: true });
+    cy.get('[data-cy="side nav link customers"]').click();
     cy.get('[data-cy="main header"]').should("have.text", "Customers Page");
     cy.get('[data-cy="side nav link customers active"]').should(
       "have.text",
       "Customers",
     );
-    cy.get('[data-cy="side nav link home"]').click({ force: true });
+    cy.get('[data-cy="side nav link home"]').click();
     cy.get('[data-cy="main header"]').should("have.text", "Dashboard");
     cy.get('[data-cy="side nav link home active"]').should("have.text", "Home");
   });
